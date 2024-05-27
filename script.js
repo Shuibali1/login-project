af388296ff1eb285ded810b32a93135224df5d71
Shuibali1 <156979104+Shuibali1@users.noreply.github.com>
2024-05-27 02:21:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === 'Login successful') {
+        window.location.href = 'gift.html'; // Redirect to gift.html
+      } else {
+        alert('Invalid credentials');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  });
+  
